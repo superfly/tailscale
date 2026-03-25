f906590536c0faf1de44b7bcbdf8eb4c19d76dd2
M. J. Fromberger <fromberger@tailscale.com>
2026-03-25 11:14:28 -0700

    ipn/ipnlocal: use node capability to regulate netmap caching
    - Switch the envknob to be a default-true explicit override.
    - Update the cache after applying the netmap successfully.
    
    Updates #todo
    
    Change-Id: I75ae3298e7e9bbf0c1f5fbb02fb2eb681d808d2f
    Signed-off-by: M. J. Fromberger <fromberger@tailscale.com>

--- wgengine/magicsock/magicsock.go
+++ wgengine/magicsock/magicsock.go
@@ -4318,7 +4318,7 @@ type NewDiscoKeyAvailable struct {
 //
 // We do not need the Conn to be locked, but the endpoint should be.
 func (c *Conn) maybeSendTSMPDiscoAdvert(de *endpoint) {
-	if !buildfeatures.HasCacheNetMap || !envknob.Bool("TS_USE_CACHED_NETMAP") {
+	if !buildfeatures.HasCacheNetMap || !envknob.BoolDefaultTrue("TS_USE_CACHED_NETMAP") {
 		return
 	}
 
